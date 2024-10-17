069b68886556fb75d6d90899714820d6068a8348
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-18 00:36:06 +0530

    feat: add Rule model

---
+++ server/RuleEngine/src/main/java/com/Zeotap/RuleEngine/rule/model/Rule.java
@@ -0,0 +1,23 @@
+package com.Zeotap.RuleEngine.rule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Component
+@Entity
+public class Rule {
+
+    @Id
+    private int id;
+    private String ruleString;
+    private Object ast;
+}
