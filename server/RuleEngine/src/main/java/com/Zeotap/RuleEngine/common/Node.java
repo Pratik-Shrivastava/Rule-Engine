56984528f5d8ee2e70899eca08aac7449f8b54e2
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-18 16:50:54 +0530

    feat: add constructor in Node class

--- server/RuleEngine/src/main/java/com/Zeotap/RuleEngine/common/Node.java
+++ server/RuleEngine/src/main/java/com/Zeotap/RuleEngine/common/Node.java
@@ -10,15 +10,15 @@ import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @Getter
 @Setter
-@Component
 public class Node {
-
-    private Long id;
 
     private String type;    // "operator" or "operand"
     private String value;   // e.g., "age > 30", "department = 'Sales'"
-
     private Node left;      // Left child node
-
     private Node right;     // Right child node
+
+    public Node(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
 }
